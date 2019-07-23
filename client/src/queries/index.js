cb07a53ae634702498756f4bb96701ad66b126bf
Tim Dill <brodt7258@gmail.com>
2019-07-23 04:21:00 -0400

    setup gql and signup for react app

---
+++ client/src/queries/index.js
@@ -0,0 +1,30 @@
+import { gql } from 'apollo-boost';
+
+/* Recipies Queries */
+
+export const GET_ALL_RECIPES = gql`
+  query {
+    getAllRecipes {
+      name
+      description
+      instructions
+      category
+      likes
+      createdDate
+    }
+  }
+`;
+
+/* Recipies Mutations */
+
+/* User Queries */
+
+/* User Mutations */
+
+export const SIGNUP_USER = gql`
+  mutation($username: String!, $email: String!, $password: String!) {
+    signupUser(username: $username, email: $email, password: $password) {
+      token
+    }
+  }
+`;
