a680c0fc33233dbc1e6390e2223b76685d656724
Tim Dill <brodt7258@gmail.com>
2019-07-24 05:10:25 -0400

    add searching

--- client/src/components/Recipe/Search.js
+++ client/src/components/Recipe/Search.js
@@ -1,7 +1,50 @@
 import React from 'react';
 
-const Search = () => (
-  <div>Search</div>
-);
+import { ApolloConsumer } from 'react-apollo';
+import { SEARCH_RECIPES } from '../../queries';
+import SearchItem from './SearchItem';
+
+class Search extends React.Component {
+  state = {
+    searchResults: []
+  }
+
+  handleChange = ({ searchRecipes }) => {
+    this.setState({
+      searchResults: searchRecipes
+    });
+  }
+
+  render() {
+    return (
+      <ApolloConsumer>
+        {client => {
+          return (
+            <div className="App">
+              <input
+                type="search"
+                placeholder="Search for Recipes"
+                onChange={async event => {
+                  event.persist();
+                  console.log(event.target.value);
+                  const { data } = await client.query({
+                    query: SEARCH_RECIPES,
+                    variables: { searchTerm: event.target.value }
+                  });
+                  this.handleChange(data);
+                }}
+              />
+              <ul>
+                {this.state.searchResults.map(recipe => (
+                  <SearchItem key={recipe._id} {...recipe} />
+                ))}
+              </ul>
+            </div>
+          );
+        }}
+      </ApolloConsumer>
+    )
+  }
+};
 
 export default Search;
